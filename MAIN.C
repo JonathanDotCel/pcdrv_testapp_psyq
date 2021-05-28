b7258522a0d7af7dee21e73727ee98a6550f5748
JonathanDotCel <wopstickle@gmail.com>
2021-05-28 19:49:39 +0100

    icons8 credit

--- MAIN.C
+++ MAIN.C
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Thanks to icons8.com for the free icons :)
+
 #include <stdlib.h>
 #include <libgte.h>
 #include <libgpu.h>
